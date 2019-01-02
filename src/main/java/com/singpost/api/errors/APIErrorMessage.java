3f8d464511de83feb46d80fa6e2bf78ebeb1ae14
nilsingh <nilesh.singh@capgemini.com>
2019-01-02 22:22:09 +0530

    added WebConfig and created IService interface in service package

--- src/main/java/com/singpost/api/errors/APIErrorMessage.java
+++ src/main/java/com/singpost/api/errors/APIErrorMessage.java
@@ -1,6 +1,33 @@
 package com.singpost.api.errors;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+@Setter
 public class APIErrorMessage
 {
+    private HttpStatus statusCode;
+    private String message;
+    private List<String> errorMessageList;
 
+    public APIErrorMessage(HttpStatus statusCode,String message,List<String> errorMessageList)
+    {
+        super();
+        this.statusCode=statusCode;
+        this.message=message;
+        this.errorMessageList=errorMessageList;
+    }
+
+    public APIErrorMessage(HttpStatus statusCode,String message,String error)
+    {
+        super();
+        this.statusCode=statusCode;
+        this.message=message;
+        this.errorMessageList= Arrays.asList(error);
+    }
 }
